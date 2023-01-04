3668184fa35d19b86d4d8218edafe08673c634ba
Nick Rodriguez <nicholasrodri92@gmail.com>
2023-01-03 23:01:45 -0600

    revise square for loop and format grid

--- etch.js
+++ etch.js
@@ -1,6 +1,6 @@
 let container = document.querySelector('.container')
 
-for(let i = 0; i <= 16; i++){
+for(let i = 0; i < 16; i++){
 let grid = document.createElement('div');
 grid.setAttribute('style', 'width: 50px; height: 50px; border: solid; margin: 10px;')
 container.appendChild(grid);
