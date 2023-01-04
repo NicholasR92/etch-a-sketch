5d72358277fd658cee32c301b804522d6b312f2e
Nick Rodriguez <nicholasrodri92@gmail.com>
2023-01-03 23:45:29 -0600

    add color randomizer to squares

--- etch.js
+++ etch.js
@@ -1,13 +1,16 @@
 let container = document.querySelector('.container')
 
+
+
 for(let i = 0; i < 256; i++){
 let grid = document.createElement('div');
-grid.setAttribute('style', 'width: 20px; height: 20px; border: solid; margin: 5px;')
+let randomColor = Math.floor(Math.random()*16777215).toString(16);
+grid.setAttribute('style', 'width: 20px; height: 20px; border: solid; margin: 2px;')
 grid.addEventListener('mouseover', (hover) => {
-    grid.style.backgroundColor = "blue";
+    grid.style.backgroundColor = "#" + randomColor;
     setTimeout(() => {
         hover.target.style.backgroundColor = "";
-      }, 500);
+      }, 1000);
     }, false);
 container.appendChild(grid);
 }
