846b45f05c1c755f37d08be0039ae45222bd737d
Nick Rodriguez <nicholasrodri92@gmail.com>
2023-01-03 23:29:29 -0600

    add function in for loop to clear out hover color

--- etch.js
+++ etch.js
@@ -5,7 +5,11 @@ let grid = document.createElement('div');
 grid.setAttribute('style', 'width: 20px; height: 20px; border: solid; margin: 5px;')
 grid.addEventListener('mouseover', (hover) => {
     grid.style.backgroundColor = "blue";
-})
+    setTimeout(() => {
+        hover.target.style.backgroundColor = "";
+      }, 500);
+    }, false);
 container.appendChild(grid);
 }
+
 
