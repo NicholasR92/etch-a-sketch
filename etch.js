9aff3e2caf0f908bd609fe294977264aaff996fd
Nick Rodriguez <nicholasrodri92@gmail.com>
2023-01-03 22:49:53 -0600

    create grid div inside container

---
+++ etch.js
@@ -0,0 +1,7 @@
+let container = document.querySelector('.container')
+
+let grid = document.createElement('div');
+grid.setAttribute('style', 'width: 50px; height: 50px; border: solid;')
+
+container.appendChild(grid);
+
