ac4c9e7edb2240093274c2d7634dbe97b744eba1
Nick Rodriguez <nicholasrodri92@gmail.com>
2023-01-03 23:03:02 -0600

    resize squares in grid

--- etch.js
+++ etch.js
@@ -2,7 +2,7 @@ let container = document.querySelector('.container')
 
 for(let i = 0; i < 16; i++){
 let grid = document.createElement('div');
-grid.setAttribute('style', 'width: 50px; height: 50px; border: solid; margin: 10px;')
+grid.setAttribute('style', 'width: 100px; height: 100px; border: solid; margin: 10px;')
 container.appendChild(grid);
 }
 
