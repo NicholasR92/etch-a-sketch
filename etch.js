a4e18b2c16079f4950b7f6f9ca76156c467b7f54
Nick Rodriguez <nicholasrodri92@gmail.com>
2023-01-04 23:00:59 -0600

    add button to top of page

--- etch.js
+++ etch.js
@@ -1,18 +1,32 @@
+let buttonContainer = document.querySelector('.buttonContainer');
 let container = document.querySelector('.container')
 
+let button = document.createElement('button');
+button.style.width = "250px";
+button.style.height = "60px"
+button.textContent = "MAKE NEW GRID"
 
+buttonContainer.appendChild(button);
 
-for(let i = 0; i < 256; i++){
-let grid = document.createElement('div');
+let gridNumber = 16;
+
+button.addEventListener('click', () => {
+    gridNumber = 0;
+})
+
+for(let i = 0; i < gridNumber * gridNumber; i++){
+let squares = document.createElement('div');
 let randomColor = Math.floor(Math.random()*16777215).toString(16);
-grid.setAttribute('style', 'width: 20px; height: 20px; border: solid; margin: 2px;')
-grid.addEventListener('mouseover', (hover) => {
-    grid.style.backgroundColor = "#" + randomColor;
+squares.setAttribute('style', 'width: 20px; height: 20px; border: solid; margin: 2px;')
+squares.addEventListener('mouseover', (hover) => {
+    squares.style.backgroundColor = "#" + randomColor;
     setTimeout(() => {
         hover.target.style.backgroundColor = "";
       }, 1000);
     }, false);
-container.appendChild(grid);
-}
+container.appendChild(squares);
+}; 
+
+
 
 
