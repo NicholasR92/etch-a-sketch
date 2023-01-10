73bd5f5acda02a6c0ce1d59500ff2f716b8f4010
Nick Rodriguez <nicholasrodri92@gmail.com>
2023-01-09 22:49:02 -0600

    add nested for loops to create grid

--- etch.js
+++ etch.js
@@ -8,25 +8,29 @@ button.textContent = "MAKE NEW GRID"
 
 buttonContainer.appendChild(button);
 
-let gridNumber = 16;
-
-button.addEventListener('click', () => {
-    gridNumber = 0;
-})
-
-for(let i = 0; i < gridNumber * gridNumber; i++){
-let squares = document.createElement('div');
-let randomColor = Math.floor(Math.random()*16777215).toString(16);
-squares.setAttribute('style', 'width: 20px; height: 20px; border: solid; margin: 2px;')
-squares.addEventListener('mouseover', (hover) => {
-    squares.style.backgroundColor = "#" + randomColor;
-    setTimeout(() => {
-        hover.target.style.backgroundColor = "";
-      }, 1000);
-    }, false);
-container.appendChild(squares);
-}; 
+let table = document.createElement('table');
+container.appendChild(table);
 
 
 
+
+let gridNumber = 64;
+
+
+for(let i = 0; i < gridNumber; i++){    
+    let rows = document.createElement('tr');
+       
+        for(let i = 0; i < gridNumber; i++){
+            let squares = document.createElement('td');
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+                squares.addEventListener('mouseover', (hover) => {
+                    squares.style.backgroundColor = "#" + randomColor;
+                    setTimeout(() => {
+                        hover.target.style.backgroundColor = "";
+                    }, 1000);
+                    }, false);
+                    rows.appendChild(squares);
+                };
+    table.appendChild(rows);
+        };
 
