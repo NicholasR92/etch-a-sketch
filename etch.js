2cc4f3fcc4b860beb2193e4e2094478ba9b975f0
Nick Rodriguez <nicholasrodri92@gmail.com>
2023-01-19 22:05:13 -0600

    add method to clear grid

--- etch.js
+++ etch.js
@@ -1,27 +1,29 @@
 let buttonContainer = document.querySelector('.buttonContainer');
 let container = document.querySelector('.container')
-
+let table = document.createElement('table');
 let button = document.createElement('button');
 button.style.width = "250px";
 button.style.height = "60px"
 button.textContent = "MAKE NEW GRID"
 
+container.appendChild(table);
 buttonContainer.appendChild(button);
 
-let table = document.createElement('table');
-container.appendChild(table);
+let gridNumber = 10;
 
 
 
 
-let gridNumber = 64;
-
+function createGrid() {
 
 for(let i = 0; i < gridNumber; i++){    
+
     let rows = document.createElement('tr');
-       
+    rows.classList.add('grid');
+
         for(let i = 0; i < gridNumber; i++){
             let squares = document.createElement('td');
+            squares.classList.add('grid');
             let randomColor = Math.floor(Math.random()*16777215).toString(16);
                 squares.addEventListener('mouseover', (hover) => {
                     squares.style.backgroundColor = "#" + randomColor;
@@ -32,5 +34,25 @@ for(let i = 0; i < gridNumber; i++){
                     rows.appendChild(squares);
                 };
     table.appendChild(rows);
-        };
+        }; 
 
+    };
+
+
+    
+    
+
+    button.addEventListener('click', () => {
+        while (table.firstChild) {
+        table.removeChild(table.firstChild)
+        }
+        gridNumber = Number(prompt("Grid Size",  ));
+        createGrid();
+
+    });
+
+   
+    createGrid();
+
+   
+    
